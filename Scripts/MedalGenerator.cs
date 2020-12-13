a33802dadd9192066cdf771bc5da07f87463048e
GamesWithJL <75936426+GamesWithJL@users.noreply.github.com>
2020-12-13 13:42:18 +0000

    Add files via upload
    All the scripts used for Trench Runner

---
+++ Scripts/MedalGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedalGenerator : MonoBehaviour
+{
+
+    public ObjectPooler medalPool;
+
+    public float distanceBetweenMedals;
+   
+    public void SpawnMedals(Vector3 startPosition)
+    {
+        GameObject medal1 = medalPool.GetPooledObject();
+        medal1.transform.position = startPosition;
+        medal1.SetActive(true);
+
+        GameObject medal2 = medalPool.GetPooledObject();
+        medal2.transform.position = new Vector3(startPosition.x - distanceBetweenMedals, startPosition.y, startPosition.z);
+        medal2.SetActive(true);
+
+        GameObject medal3 = medalPool.GetPooledObject();
+        medal3.transform.position = new Vector3(startPosition.x + distanceBetweenMedals, startPosition.y, startPosition.z);
+        medal3.SetActive(true);
+    }
+}
